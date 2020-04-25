767ce4b7de65c444ec2ae0f6d9942836ea2a221a
HedgeHawk <darth-sidious@gmx.net>
2020-04-25 11:17:02 +0200

    Hedge hawk patch 1 (#1)
    * Create run.sh
    
    * Create Dockerfile

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#dump1090 runtime directory for json output
+mkdir -p /run/dump1090-fa
+chmod 755 /run/dump1090-fa
+
+#start webserver
+/etc/init.d/lighttpd start
+
+CUSTOM_OPTIONS=""
+if [ -n "${RECEIVER_LATITUDE}" ]; then
+  CUSTOM_OPTIONS="${CUSTOM_OPTIONS} --lat ${RECEIVER_LATITUDE}"
+fi
+
+if [ -n "${RECEIVER_LONGITUDE}" ]; then
+  CUSTOM_OPTIONS="${CUSTOM_OPTIONS} --lon ${RECEIVER_LONGITUDE}"
+fi
+
+#start dump1090
+/usr/share/dump1090-fa/start-dump1090-fa --write-json /run/dump1090-fa --quiet ${CUSTOM_OPTIONS}
